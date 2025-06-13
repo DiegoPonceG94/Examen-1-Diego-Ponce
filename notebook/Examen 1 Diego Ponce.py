26b09122c8a68936064002c36eeeac219307e04d
Diego Ponce <diegoponce31@gmail.com>
2025-06-13 19:00:26 -0400

    Modificacion archivos de carpetas

---
+++ notebook/Examen 1 Diego Ponce.py
@@ -0,0 +1,13 @@
+# -----------------------------------------
+# 1. Preparación del Entorno
+# -----------------------------------------
+
+# Usamos la biblioteca google para poder usar archivos en nuestro drive.
+from google.colab import drive
+# Este comando conecta colab con drive.
+drive.mount('/content/drive')
+
+# Importamos bibliotecas
+import pandas as pd
+import numpy as pd
+import matplotlib.pyplot as pd
